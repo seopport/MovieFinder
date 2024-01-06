d1a678da41f409e573d2c2499c264193f1e21b97
Jiwon <tj1tj2tj3@naver.com>
2024-01-06 23:46:33 +0900

    css realfinish

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+// curl --request GET \
+//      --url 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1' \
+//      --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjlhYmQyZjU5MTYxMTg2ZmUwNzZhMGJmMzA2ZTcxOSIsInN1YiI6IjY1OTk1Nzg5YmQ1ODhiMDIwNDU3NTU1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.014IzV6JFQswTqZDfeIjHDud_khco-fa3a_GOd4V2gE' \
+//      --header 'accept: application/json'
